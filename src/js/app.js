f6b5b6352d3975368b6ad82882795fdd3701fa31
Elias <eliassjogreen1@gmail.com>
2017-10-20 18:53:18 +0200

    Added support for clay.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
 var UI = require('ui');
 var ajax = require('ajax');
 
+var Settings = require('settings');
+var Clay = require('clay');
+var clayConfig = require('config');
+var clay = new Clay(clayConfig);
+
+var options = Settings.option();
+
 var node = {id: 'ff67240e523fc15e2fbdf4caeed8421840e0f71c', nick: 'NAS'};
+
 var interval = 10000;
+
 var main = new UI.Menu({
     sections: [{
 		title: 'STORJ Node Viewer'
@@ -12,7 +21,8 @@ var main = new UI.Menu({
 main.section(1, { title: node.nick, items: [
 	{title: 'Node Id'}, 
 	{title: 'Space available'}, 
-	{title: 'Ip'}, {title: 'Port'}, 
+	{title: 'Ip'}, 
+	{title: 'Port'}, 
 	{title: 'Reputation'}, 
 	{title: 'Response time'}, 
 	{title: 'Last seen'}, 
@@ -21,22 +31,62 @@ main.section(1, { title: node.nick, items: [
 	{title: 'User agent'}
 ]});
 
-setInterval(function() {
-		ajax({ url: 'https://api.storj.io/contacts/' + node.id, type: 'json'},
-		function(data) {
-			main.items(1, [ { title: 'Node Id', subtitle: node.id},
-							{ title: 'Space available', subtitle: data.spaceAvailable.toString()},
-							{ title: 'Ip', subtitle: data.address},
-							{ title: 'Port', subtitle: data.port},
-							{ title: 'Reputation', subtitle: data.reputation},
-							{ title: 'Response time', subtitle: data.responseTime.toString()},
-							{ title: 'Last seen', subtitle: new Date(data.lastSeen).toDateString() + ' ' + new Date(data.lastSeen).getHours() + ':' + new Date(data.lastSeen).getMinutes()},
-							{ title: 'Last timeout', subtitle: new Date(data.lastTimeout).toDateString() + ' ' + new Date(data.lastTimeout).getHours() + ':' + new Date(data.lastTimeout).getMinutes()},
-							{ title: 'Timeout rate', subtitle: data.timeoutRate},
-							{ title: 'Protocol', subtitle: data.protocol},
-							{ title: 'User agent', subtitle: data.userAgent}
-							]);
-		});
-}, interval);
-
 main.show();
+
+Pebble.addEventListener('showConfiguration', function(e) {
+	var claySettings = JSON.parse(localStorage.getItem("clay-settings"));
+	for (var key in claySettings) {
+		if (claySettings.hasOwnProperty(key)) {
+			if(key in options){
+				claySettings[key] = options[key];
+			}
+		}
+	}	
+	localStorage["clay-settings"] = JSON.stringify(claySettings);
+  	Pebble.openURL(clay.generateUrl());
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {	
+	if (e && !e.response) {
+		return;
+	}
+	
+	var dict = clay.getSettings(e.response);
+	
+	Settings.option(dict);
+	options = Settings.option();
+	console.log(JSON.stringify(options));
+	
+	setOptions();
+});
+
+function setOptions() {
+	interval = options.interval;
+	node.id = options.id;
+	node.nick = options.nick;
+	
+	updateItems();
+}
+
+function updateItems() {
+	ajax({ url: 'https://api.storj.io/contacts/' + node.id, type: 'json'},
+			function(data) {
+				main.items(1, [ 
+					{ title: 'Node Id', subtitle: node.id},
+					{ title: 'Space available', subtitle: data.spaceAvailable.toString()},
+					{ title: 'Ip', subtitle: data.address},
+					{ title: 'Port', subtitle: data.port},
+					{ title: 'Reputation', subtitle: data.reputation},
+					{ title: 'Response time', subtitle: data.responseTime.toString()},
+					{ title: 'Last seen', subtitle: new Date(data.lastSeen).toDateString() + ' ' + new Date(data.lastSeen).getHours() + ':' + new Date(data.lastSeen).getMinutes()},
+					{ title: 'Last timeout', subtitle: new Date(data.lastTimeout).toDateString() + ' ' + new Date(data.lastTimeout).getHours() + ':' + new Date(data.lastTimeout).getMinutes()},
+					{ title: 'Timeout rate', subtitle: data.timeoutRate},
+					{ title: 'Protocol', subtitle: data.protocol},
+					{ title: 'User agent', subtitle: data.userAgent}
+				]);
+			});
+}
+
+setInterval(function() {
+	updateItems();
+}, interval);
---
+++ src/js/config.js
@@ -0,0 +1,55 @@
+module.exports = [
+	{ 
+    	"type": "heading", 
+    	"defaultValue": "STORJPebble" 
+  	}, 
+  	{ 
+    	"type": "text", 
+    	"defaultValue": "View the project source there -> https://github.com/eliassjogreen/STORJPebble" 
+  	},
+	{
+		"type": "section",
+		"items": [
+			{
+      			"type": "heading",
+      			"defaultValue": "Settings"
+    		},
+			{
+				"type": "input",
+				"appKey": "interval",
+				"defaultValue": "10000",
+				"label": "Refresh interval (ms)",
+				"attributes": {
+					"placeholder": "10000",
+					"required": "required",
+					"type": "number"
+				}
+			},
+			{
+				"type": "input",
+				"appKey": "nick",
+				"label": "Node Nickname",
+				"attributes": {
+					"placeholder": "NAS",
+					"required": "required",
+					"type": "text"
+				}
+			},
+			{
+				"type": "input",
+  				"appKey": "id",
+				"label": "Node Id",
+				"attributes": {
+					"placeholder": "ff67240e523fc15e2fbdf4caeed8421840e0f71c",
+					"limit": 40,
+					"required": "required",
+					"type": "text"
+				}
+			}
+		]
+	},
+	{
+		"type": "submit",
+		"defaultValue": "Save"
+	}
+];
